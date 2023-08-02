7fb0b0870ac5c73b806eaffac72c134cf11ec55a
wangss <2653286639@qq.com>
2023-08-02 18:31:34 +0800

    版本1

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author wangss
+ * @create 2023-08-02 18:27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello! git!");
+    }
+}
