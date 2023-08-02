0e0239dd7dec7c70d1afb7283433f9406ff720ef
wangss <2653286639@qq.com>
2023-08-02 20:13:23 +0800

    push之后版本1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello!hot-fix!");
         System.out.println("hello!hot-fix 2!");
         System.out.println("hello!master!");
+        System.out.println("push test!");
     }
 }
